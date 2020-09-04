422d97696e814c51a17c2ff5d3d7b63069ddbf6d
soniajohari <54836162+soniajohari@users.noreply.github.com>
2020-09-04 15:42:40 +0800

    Created user.sql
    has userinfo database
    with user and epoints table

---
+++ Database/user.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 14, 2019 at 06:42 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `userinfo`
+--
+CREATE DATABASE IF NOT EXISTS `userinfo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `userinfo`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`email`, `password`, `username`) VALUES
+('alice@aaa.com', '123', 'alice'),
+('bob@bbb.com', '234', 'bob'),
+('calvin@ccc.com', '345', 'calvin')
+
+COMMIT;
+
+--
+-- Table structure for table `epoints`
+--
+
+DROP TABLE IF EXISTS `epoints`;
+CREATE TABLE IF NOT EXISTS `epoints` (
+  `email` varchar(255) NOT NULL,
+  `epoints` int(255) NOT NULL
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `epoints`
+--
+
+INSERT INTO `epoints` (`email`, `epoints`) VALUES
+('alice@aaa.com', 10),
+('bob@bbb.com', 20),
+('calvin@ccc.com', 30)
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
