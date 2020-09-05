64b290fe370e02e8409393ea257ffb0d3f2ead00
Vedant20021997 <vedantm.2018@smu.edu.sg>
2020-09-06 01:06:35 +0800

    Removed list formatting from header.php

--- Frontend/header.php
+++ Frontend/header.php
@@ -1,12 +1,12 @@
 <html>
 <div>
         <ul id="menu" align=right>
-            <li><a href='Home.php'> Home </a></li>
-            <li><a href='Risk.php'> Risk Appetite </a></li>
-            <li><a href='Assets.php'> Assets & Liability </a></li>
-            <li><a href='Short.php'> Short-Term Investment </a></li>
-            <li><a href='Long.php'> Long-Term Investment </a></li>
-            <li><a href='Logout.php'><font color="red">Logout </font></a></li>
+            <a href='Home.php'> Home</a>
+            <a href='Risk.php'> Risk Appetite</a>
+            <a href='Assets.php'> Assets & Liability</a>
+            <a href='Short.php'> Short-Term Investment</a>
+            <a href='Long.php'> Long-Term Investment</a>
+            <a href='Logout.php'><font color="red">Logout </font></a>
         </ul>
 </div>
 </html>
