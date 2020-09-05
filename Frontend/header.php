81a1d646e98757f92ca68f430d69235152ede980
Vedant20021997 <vedantm.2018@smu.edu.sg>
2020-09-06 00:18:47 +0800

    header.php

---
+++ Frontend/header.php
@@ -0,0 +1,12 @@
+<html>
+<div>
+        <ul id="menu" align=right>
+            <li><a href='Home.php'> Home </a></li>
+            <li><a href='Risk.php'> Risk Appetite </a></li>
+            <li><a href='Assets.php'> Assets & Liability </a></li>
+            <li><a href='Short.php'> Short-Term Investment </a></li>
+            <li><a href='Long.php'> Long-Term Investment </a></li>
+            <li><a href='Logout.php'><font color="red">Logout </font></a></li>
+        </ul>
+</div>
+</html>
