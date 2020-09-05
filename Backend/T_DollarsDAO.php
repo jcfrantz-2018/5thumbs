f9f92ea2544f72e274e982a2420a9ff916db274a
Vedant20021997 <vedantm.2018@smu.edu.sg>
2020-09-05 16:59:52 +0800

    T_DollarsDAO.php
    addUserT_Dollar.php

--- Backend/T_DollarsDAO.php
+++ Backend/T_DollarsDAO.php
@@ -34,5 +34,35 @@ class T_DollarsDAO {
         // Return (if any)
         return $result;
     }
+    
+    public function addUserT_Dollar($email, $username) {
+        $sql = 'INSERT INTO t_dollars (email, username, T_Dollars) 
+                VALUES (:email, :username, 100)';
+    
+        $connMgr = new ConnectionManager();       
+        $conn = $connMgr->getConnection();
+        
+        $stmt = $conn->prepare($sql); 
 
+        $stmt->bindParam(':email', $email , PDO::PARAM_STR);
+        $stmt->bindParam(':username', $username , PDO::PARAM_STR);
+
+        $isAddOK = False;
+        if ($stmt->execute()) {
+            $isAddOK = True;
+        }
+
+        if ($isAddOK === False) {
+            
+            $stmt = null;
+            $pdo = null;
+
+            return $stmt->errorInfo();
+        }
+
+        $stmt = null;
+        $pdo = null;
+
+        return $isAddOK;
+    }
 }
