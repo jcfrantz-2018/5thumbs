a7075c80b1ec9b5f1ae0d6fe0eeacd6d4ae2fd50
Veronica <xuanhui.lee.2018@sis.smu.edu.sg>
2020-09-06 21:45:23 +0800

    included get_market_summary() and get_stock()

--- Backend/FinanceAPI.php
+++ Backend/FinanceAPI.php
@@ -1,42 +1,83 @@
 <?php
 
-function FinanceAPI(){
+function get_market_summary(){
 
-$curl = curl_init();
+	$curl = curl_init();
 
 
-curl_setopt_array($curl, array(
-	CURLOPT_URL => "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en",
-	CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_SSL_VERIFYHOST => false, 
-    CURLOPT_SSL_VERIFYPEER => false, 
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => array(
-		"x-rapidapi-host: apidojo-yahoo-finance-v1.p.rapidapi.com",
-		"x-rapidapi-key: eab7cb3d4fmsh7218e7d8a32d07dp131570jsn4a3ac98cce01"
-	),
-));
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_SSL_VERIFYHOST => false, 
+		CURLOPT_SSL_VERIFYPEER => false, 
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+			"x-rapidapi-host: apidojo-yahoo-finance-v1.p.rapidapi.com",
+			"x-rapidapi-key: eab7cb3d4fmsh7218e7d8a32d07dp131570jsn4a3ac98cce01"
+		),
+	));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
 
-curl_close($curl);
+	curl_close($curl);
 
-if ($err) {
-	$data = $err; 
-} else {
-	$data =  $response;
+	if ($err) {
+		$data = $err; 
+	} else {
+		$data =  $response;
+	}
+
+	return $data; 
 }
 
-return $data; 
-}
 
-$SESSION['FinanceAPI'] = FinanceAPI(); 
+
+
+
+function get_stock(){
+
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-detail?region=US&lang=en&symbol=NBEV",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_SSL_VERIFYHOST => false, 
+		CURLOPT_SSL_VERIFYPEER => false, 
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+			"x-rapidapi-host: apidojo-yahoo-finance-v1.p.rapidapi.com",
+			"x-rapidapi-key: eab7cb3d4fmsh7218e7d8a32d07dp131570jsn4a3ac98cce01"
+		),
+	));
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+		$data = $err; 
+	} else {
+		$data =  $response;
+	}
+	return $data; 
+		}
+
+		
+$_SESSION['market_summary'] = get_market_summary(); 
+$_SESSION['stocks'] = get_stock(); 
 
 
 ?>
